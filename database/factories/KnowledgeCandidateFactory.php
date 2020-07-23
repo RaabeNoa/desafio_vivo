563ec6acd07626b0c1774af5942c16935f2579a0
raabe <raabe@explicae.com.br>
2020-07-23 16:26:53 -0300

    Aplicação - migration e factories

---
+++ database/factories/KnowledgeCandidateFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\KnowledgeCandidate;
+use Faker\Generator as Faker;
+
+$factory->define(KnowledgeCandidate::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
