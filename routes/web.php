c07378d5f703c3d59f4d47af222d8b327bf329f1
raabe <raabe@explicae.com.br>
2020-07-23 16:29:26 -0300

    Add novas rotas

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,19 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 */
-//Route::get('/', function () {
-//    return view('knowledge.index');
-//});
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
 
 Route::group(['prefix' => 'knowledge'], function ()  {
     Route::get('', 'KnowledgeController@index')->name('list_knowledge');
     Route::get('/add', 'KnowledgeController@create')->name('form_add_knowledge');
     Route::post('/add', 'KnowledgeController@store')->name('add_knowledge');
     Route::delete('{id}', 'KnowledgeController@destroy')->name('delete_knowledge');
+});
+
+Route::group(['prefix' => 'applications'], function ()  {
+    Route::get('', 'ApplicationController@index')->name('list_applications');
+    Route::get('/apply', 'ApplicationController@create')->name('form_application');
+    Route::post('/apply', 'ApplicationController@store')->name('add_application');
 });
