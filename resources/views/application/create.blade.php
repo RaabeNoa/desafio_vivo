d464c452b43c9ec930978fd49d0c5db4fe426aba
raabe <raabe@explicae.com.br>
2020-07-23 16:30:01 -0300

    Aplicação - add resources

--- resources/views/application/create.blade.php
+++ resources/views/application/create.blade.php
@@ -1,1 +1,35 @@
-<?php
+@extends('layout')
+@section('title')
+    Aplicação Vivo
+@endsection
+@section('header')
+    Aplicar - Vaga Desenvolvedor
+@endsection
+@section('content')
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form method="post">
+        @csrf
+        <div class="form-group">
+            <label for="name">Nome</label>
+            <input type="text" class="form-control" name="name" id="name">
+            <label for="email">Email</label>
+            <input type="text" class="form-control" name="email" id="email">
+            <label for="knowledge-list">Avalie seu conhecimento de 0 a 10 nos seguintes itens: </label>
+            <ul class="list-group" name="knowledge-list">
+                @foreach($knowledge as $k)
+                    <li class="list-group-item d-flex justify-content-between align-items-center">{{ $k->name }}</li>
+                    <input type="text" class="form-control" name="grade" id="grade">
+                @endforeach
+            </ul>
+        </div>
+        <button class="btn btn-primary">Adicionar</button>
+    </form>
+@endsection
--- resources/views/application/index.blade.php
+++ resources/views/application/index.blade.php
@@ -1,1 +1,19 @@
-<?php
+@extends('layout')
+@section('title')
+    Candidaturas
+@endsection
+@section('header')
+    Lista de Candidaturas
+@endsection
+@section('content')
+    @if(!empty($message))
+        <div class="alert alert-success">
+            {{ $message }}
+        </div>
+    @endif
+    <ul class="list-group">
+        @foreach($application as $ap)
+            <li class="list-group-item"> {{ $ap->name }} - {{$ap->email}} </li>
+        @endforeach
+    </ul>
+@endsection
