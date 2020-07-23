9f0e35c6379ab3c94f736b47b82dba751837068b
raabe <raabe@explicae.com.br>
2020-07-23 13:24:00 -0300

    Conhecimento - Add migration e factory

---
+++ database/factories/KnowledgeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Knowledge;
+use Faker\Generator as Faker;
+
+$factory->define(Knowledge::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
