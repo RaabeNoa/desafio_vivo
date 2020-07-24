0f8b6b253b05a874ce36bf0fbe74d5255ce7afe1
raabe <raabe@explicae.com.br>
2020-07-23 23:07:01 -0300

    Add repository

--- app/Repositories/KnowledgeApplicationRepository.php
+++ app/Repositories/KnowledgeApplicationRepository.php
@@ -4,7 +4,16 @@
 namespace App\Repositories;
 
 
+use Illuminate\Support\Facades\DB;
+
 class KnowledgeApplicationRepository
 {
-
+    public function chooseEmails($request)
+    {
+        DB::transaction(function () use ($request) {
+            //switch ($request->grade){
+                /* TODO: Verificar array $request->grades e retornar quais emails devem ser enviados  */
+           // }
+        });
+    }
 }
